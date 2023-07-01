309eaa9eafd079b1209c0f1a0250ea1de86d7be6
liuyuanbo <1131107655@qq.com>
2023-07-01 14:58:18 +0800

    Recommend-Rank-Like页面搭建完成

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,14 +2,20 @@
   <div class="home">
     <type-nav></type-nav>
     <list-container></list-container>
+    <recommend></recommend>
+    <Rank></Rank>
+    <like></like>
   </div>
 </template>
 
 <script>
 import ListContainer from './ListContainer.vue'
 import TypeNav from '../components/TypeNav.vue'
+import Recommend from './Recommend.vue'
+import Rank from './Rank.vue'
+import Like from './Like.vue'
 export default {
-  components: { TypeNav, ListContainer },
+  components: { TypeNav, ListContainer,Recommend,Rank, Like, },
 name:'Home'
 }
 </script>
