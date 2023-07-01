e301e42384377f2e357198fad1ce2757bf05518c
liuyuanbo <1131107655@qq.com>
2023-07-01 15:35:37 +0800

    Floor-Brand静态页面搭建完成

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,6 +5,9 @@
     <recommend></recommend>
     <Rank></Rank>
     <like></like>
+    <floor></floor>
+    <floor></floor>
+    <brand></brand>
   </div>
 </template>
 
@@ -14,8 +17,10 @@ import TypeNav from '../components/TypeNav.vue'
 import Recommend from './Recommend.vue'
 import Rank from './Rank.vue'
 import Like from './Like.vue'
+import Floor from './Floor.vue'
+import Brand from './Brand.vue'
 export default {
-  components: { TypeNav, ListContainer,Recommend,Rank, Like, },
+  components: { TypeNav, ListContainer,Recommend,Rank, Like, Floor, Brand, },
 name:'Home'
 }
 </script>
