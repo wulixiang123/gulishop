42ad1c590e42514bde249db8c0e9aa545fc00860
liuyuanbo <1131107655@qq.com>
2023-07-06 09:29:16 +0800

    更新注释

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import store from './store'
 import '@/mock'
 
+// Vue.js中的全局注册是指将组件注册到全局，
+// 这样在任何新创建的Vue根实例(new Vue)的模板中都可以使用。
+// 全局注册通过Vue.component来创建组件
 Vue.config.productionTip = false
 // 全局注册
 import TypeNav from '@/components/TypeNav.vue'
