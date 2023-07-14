1c29687c05cb46538ac48aea829b9b3498eb895a
liuyuanbo <1131107655@qq.com>
2023-07-14 19:02:37 +0800

    组件通信-.sync

--- src/pages/Communication/comp/05_sync/Child1.vue
+++ src/pages/Communication/comp/05_sync/Child1.vue
@@ -1,12 +1,22 @@
 <template>
     <div class="box">
-
+        <h2>Child1组件</h2>
+        <div>qwer: {{ qwer }}</div>
+        <button @click="xxxHandler">修改 qwer 数据</button>
     </div>
 </template>
 
 <script>
 export default {
     name: "Child1",
+    props: ["qwer"],
+    methods: {
+        xxxHandler() {
+            let text = this.qwer + '燕子'
+            // this.$emit('changeXXX',  text)
+            this.$emit('update:qwer',  text)
+        }
+    }
 }
 </script>
 
