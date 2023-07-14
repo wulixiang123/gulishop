8c46a46f225d330aebff3829d7d023092407e0d5
liuyuanbo <1131107655@qq.com>
2023-07-14 19:03:14 +0800

    组件通信-$attrs_$listeners

--- src/pages/Communication/comp/06_attrs-listeners/AttrsListenersTest.vue
+++ src/pages/Communication/comp/06_attrs-listeners/AttrsListenersTest.vue
@@ -2,12 +2,42 @@
     <div class="box">
         <h1>组件间通信: $attrs与$listeners</h1>
         <h2>需求: 自定义带Hover提示的按钮</h2>
+
+        <br>
+
+        <HintButton type="primary" content="添加" class="abc" style="color: red;" name='asdf' id="zxc"></HintButton>
+
+
+        <HintButton
+            type="success"
+            content="删除"
+            size="mini"
+            round
+            @click="clickHandler"
+            @abc="abcHandler"
+        ></HintButton>
     </div>
 </template>
 
 <script>
+// $attrs
+// 获取到绑定在组件上的所有属性(包括属性值),除了props接收过的属性 和 class 和 style
+// 可以使用 v-bind="$attrs" 将所有接收到的属性直接绑定在元素上,这样就不需要一个一个接收的绑定了
+// $listeners
+// 接收所有绑定在组建上的事件
+// 可以用于直接给子组件中某个绑定事件,绑定的事件是写在组件标签上的事件
+import HintButton from './HintButton.vue'
 export default {
     name: "AttrsListenersTest",
+    components: { HintButton },
+    methods: {
+        clickHandler() {
+            console.log('触发了组件的click')
+        },
+        abcHandler() {
+            console.log('触发了组件的abc')
+        }
+    }
 }
 </script>
 
--- src/pages/Communication/comp/06_attrs-listeners/HintButton.vue
+++ src/pages/Communication/comp/06_attrs-listeners/HintButton.vue
@@ -1,15 +1,30 @@
 <template>
-    <div>
-        
-    </div>
+    <el-popover
+        placement="top"
+        trigger="hover"
+        :content="content"
+    >
+        <!-- <el-button slot="reference" v-bind="$attrs" @click="btnHandler">{{ content }}</el-button> -->
+        <el-button slot="reference" v-bind="$attrs" v-on="$listeners">{{ content }}</el-button>
+    </el-popover>
 </template>
 
 <script>
 export default {
-    name:"HintButton",
-}
+    name: "HintButton",
+    // props: ['type', 'content', 'size', 'round'],
+    props: ['content'],
+    mounted() {
+        console.log(this.$attrs)
+        console.log(this.$listeners)
+    },
+    methods: {
+        btnHandler() {
+            console.log('触发了按钮的btn')
+        }
+    }
+};
 </script>
 
 <style scoped>
-
 </style>
