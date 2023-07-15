ab3bbaa5fe32b0fa46f43a37312d877e9e619866
liuyuanbo <1131107655@qq.com>
2023-07-15 10:57:28 +0800

    provide-inject

--- src/pages/Communication/comp/09_provide-inject/GrandChild.vue
+++ src/pages/Communication/comp/09_provide-inject/GrandChild.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="box">
         <h4>孙组件标题</h4>
+        <div>dream1: {{ dream1 }}</div>
+        <div>dream2.name: {{ dream2.name }}</div>
     </div>
 </template>
 
 <script>
 export default {
     name: "GrandChild",
+    inject: ['dream1', 'dream2']
 };
 </script>
--- src/pages/Communication/comp/09_provide-inject/ProvideInjectTest.vue
+++ src/pages/Communication/comp/09_provide-inject/ProvideInjectTest.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="box">
+        <div>contnet1:{{ content1 }}</div>
+        <div>contnet2.name:{{ content2.name }}</div>
+        <button @click="changeContent1">修改content1</button>
+        <button @click="changeContent2">修改content2</button>
+        <child></child>
     </div>
 </template>
 
@@ -8,7 +13,7 @@ import Child from "./Child";
 export default {
     name: "ProvideInjectTest",
     components: {
-        Child,
+        Child
     },
     data() {
         return {
@@ -18,6 +23,24 @@ export default {
             },
         };
     },
+    provide(){
+        return{
+            dream1:this.content1,
+            dream2:this.content2,
+        }
+    },
+    methods: {
+        changeContent1() {
+            this.content1 = 'rose'
+        },
+        changeContent2() {
+            // this.content2 = {
+            //     name: 'jerry'
+            // }
+
+            this.content2.name = 'Tony'
+        }
+    }
 };
 </script>
 
